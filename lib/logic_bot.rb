8ae16eca3aa485c93a6e1c739445df5635a40c87
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-05 12:16:10 +0300

    add check_winner method

--- lib/logic_bot.rb
+++ lib/logic_bot.rb
@@ -11,6 +11,7 @@ class GameLogic
     @player = Player.new
     @bot_player = Player.new('smart_bot')
     @players = [@player, @bot_player]
+    @winner = 'DRAW'
   end
 
   def play?(bool = nil)
@@ -53,16 +54,46 @@ class GameLogic
   end
 
   def make_move(choice = nil)
-    if (bot_player.symbol == 'X')
-      bot_player.make_choice
-      player.make_choice(choice)
+    unless check_winner
+      if (bot_player.symbol == 'X')
+        bot_player.make_choice(bot_logic_choice)
+        player.make_choice(choice)
+      else
+        player.make_choice(choice)
+        bot_player.make_choice(bot_logic_choice)
+      end
     else
-      player.make_choice(choice)
-      bot_player.make_choice
+
+  end
+
+  def check_winner
+    answers = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    answers.each do |sub_array|
+      intersection = sub_array & player.answers
+      intersection2 = sub_array & bot_player.answers
+      if intersection == sub_array
+        @play = true
+        @winner = player.name
+        return true
+      elsif intersection2 == sub_array
+        @play = true
+        @winner = bot_player.name
+        return true
+      elsif bot_player.answers.length >= 5 || player.answers.length >= 5
+        @play = false
+        @winner = 'DRAW'
+        return true
+      end
     end
+    return false
   end
 
   def bot_logic_choice
-    
+    # if bot_player.symbol = 'X'
+      
+
+    # else
+    return 5
+
   end
 end
