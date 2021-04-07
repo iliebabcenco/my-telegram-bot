26c1260e6fa4c6c67732042a8859694da1a8f374
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-07 16:21:06 +0300

    Unhide hide_token.rb file, change require to require_relative for local files

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,8 @@
 require 'telegram/bot'
-require './lib/ui_tictac'
-require './lib/logic_bot'
-require './lib/player'
-require './lib/hide_token'
+require_relative './lib/ui_tictac'
+require_relative './lib/logic_bot'
+require_relative './lib/player'
+require_relative './lib/hide_token'
 
 token = TOKEN
 
@@ -41,6 +41,8 @@ Telegram::Bot::Client.run(token) do |bot|
   player = game_logic.player
   bot.listen do |message|
     player.name = message.from.first_name
+    time = Time.now
+    p "#{player.name} is now started the game"
     mscounter += 1
     if mscounter == 1
       key_type = %w[yes no]
@@ -67,17 +69,21 @@ Telegram::Bot::Client.run(token) do |bot|
         game_logic = interface.game_logic
         player = game_logic.player
         send_keyboard(bot, message, 'Start!', answer)
+        p "player #{player.name} is at game over at #{Time.now - time}"
       when 'end'
         interface = UIgame.new
         game_logic = interface.game_logic
         player = game_logic.player
         send_keyboard(bot, message, 'Start!', interface.finish_game)
+        p "player #{player.name} is at end at #{Time.now - time}"
       when 'numbers-error'
         send_keyboard(bot, message, interface.key_board,
                       "'#{message.text}' is not a good choice please select an available number from the board.")
         send_message(bot, message, interface.draw_board)
+        p "player #{player.name} is at numbers-error at #{Time.now - time}"
       when 'error'
         send_message(bot, message, ANSWERS.sample)
+        p "player #{player.name} is at error at #{Time.now - time}"
       end
     end
   end
